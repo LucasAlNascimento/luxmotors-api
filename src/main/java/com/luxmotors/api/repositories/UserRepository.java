ec746fbdd9a889306ae947063d264f6e59a65ee0
LucasAlNascimento <lucasalvesnascimento.lan@gmail.com>
2026-06-28 00:40:41 -0300

    fix: mapeamento correto entidade

--- src/main/java/com/luxmotors/api/repositories/UserRepository.java
+++ src/main/java/com/luxmotors/api/repositories/UserRepository.java
@@ -2,12 +2,10 @@ package com.luxmotors.api.repositories;
 
 import com.luxmotors.api.domain.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    Optional<UserDetails> findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
-
